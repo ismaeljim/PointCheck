981e22f2d7d45901bb9e922e3c0ba979c6ba09d2
ismaeljim <ismael.jimenezr13@gmail.com>
2026-04-20 19:12:43 -0400

    Add Spring Boot backend

---
+++ backend/src/test/kotlin/com/duoc/app/PointCheckApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.duoc.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PointCheckApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
