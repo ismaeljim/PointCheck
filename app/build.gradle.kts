27ab5cd585c9ef2d67780effa1f096db86bd3ff4
ismaeljim <ismael.jimenezr13@gmail.com>
2026-04-30 18:06:11 -0400

    eliminacion de room como caché local ahora se usa datastore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,11 +54,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 
-    // Room con KSP (NO KAPT)
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1") // 🔥 reemplaza a kapt
-
     // Corrutinas
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
