aa7ed656c901072df1ad30da25c346cdcdc1c4d7
ismaeljim <ismael.jimenezr13@gmail.com>
2026-04-30 18:06:11 -0400

    eliminacion de room como caché local ahora se usa datastore

--- app/src/main/java/com/pointcheck/features/services/presentation/ServiceListScreen.kt
+++ app/src/main/java/com/pointcheck/features/services/presentation/ServiceListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
@@ -29,7 +30,7 @@ fun ServiceListScreen(
                 title = { Text("Mis Servicios") },
                 navigationIcon = {
                     IconButton(onClick = { nav.popBackStack() }) {
-                        Icon(androidx.compose.material.icons.Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Volver")
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Volver")
                     }
                 }
             )
